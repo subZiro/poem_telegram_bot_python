4319f99ba5b2f6085e4cd6a0e79dae705ab8a37c
SubZiro <webnlevel@gmail.com>
2020-01-21 17:14:37 +0400

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import telebot
 from telebot import apihelper
 import sqlite3
@@ -7,43 +9,36 @@ from time import sleep
 import os
 import sys
 
-#sys.path.append("../dataset_for_poem_TG_bot_python")
-#sys.path.append("dataset_for_poem_TG_bot_python")
-
+# sys.path.append("../dataset_for_poem_TG_bot_python")
+# sys.path.append("dataset_for_poem_TG_bot_python")
 import db
-
 from config import TG_TOKEN, TG_PROXY_URL, TG_ADMIN_ID
 
-
-
-
-
-apihelper.proxy = {'https' : TG_PROXY_URL}
+apihelper.proxy = {'https': TG_PROXY_URL}
 bot = telebot.TeleBot(TG_TOKEN)
 
-
-
-#--commands for user--#
+# --commands for user--#
 commands = {  # command description used in the "help" command
-    'start'		: 'Начало использования бота',
-    'help'		: 'Информация о доступных командах',
-    'random'		: 'Получить случайное стихотворение'
-    }
+    'start': 'Начало использования бота',
+    'help': 'Информация о доступных командах',
+    'random': 'Получить случайное стихотворение',
+    'restart': 'Сброс всей статистики'
+}
 
 
 def auth(func):
-	# защита от пользования ботом другими пользователями
-	def wrapper(message):
-		if message.chat.id == TG_ADMIN_ID:
-			return func(message)
-		else:
-			bot.send_message(message.chat.id, 'У вас нет доступа')
-			return False
+    # защита от пользования ботом другими пользователями
+    def wrapper(message):
+        if message.chat.id == TG_ADMIN_ID:
+            return func(message)
+        else:
+            bot.send_message(message.chat.id, 'У вас нет доступа')
+            return False
 
-	return wrapper
+    return wrapper
 
 
-#--start use bot--#
+# --start use bot--#
 @auth
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -51,8 +46,7 @@ def welcome(message):
     user_id = message.chat.id
 
 
-
-#--help page--#
+# --help page--#
 @auth
 @bot.message_handler(commands=['help'])
 def command_help(message):
@@ -64,50 +58,36 @@ def command_help(message):
     bot.send_message(user_id, help_text)  # send the generated help page
 
 
-
-#--get random poem--#
+# --get random poem--#
 @auth
 @bot.message_handler(commands=['random'])
 def command_random(message):
-	#
     user_id = message.chat.id
-
     d1 = db.f_select_random_poem()
-
     bot.send_message(user_id, d1)  # show the keyboard
-    
-   
-
-
-
 
 
 @auth
 @bot.message_handler(content_types=["text"])
-def send_text(message): # Название функции не играет никакой роли, в принципе
-	#
-	if message.text.lower() == 'привет':
-		bot.send_message(message.chat.id, 'Приветствую тебя')
-	elif message.text.lower() == 'пока':
-		bot.send_message(message.chat.id, 'Прощай, но ты заходи хоть иногда')
-	elif message.text.lower() == 'хуй':
-		bot.send_message(message.chat.id, f'{message.chat.first_name} соси нигерскую письку')
-	else:
-		bot.send_message(message.chat.id, message.text)
+def send_text(message):
+    """Название функции не играет никакой роли, в принципе
+	"""
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Приветствую тебя')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, но ты заходи хоть иногда')
+    elif message.text.lower() == 'хуй':
+        bot.send_message(message.chat.id, f'{message.chat.first_name} соси нигерскую письку')
+    else:
+        bot.send_message(message.chat.id, message.text)
 
 
-
-
-
-
-if __name__ == '__main__' :
-	try:
-		print('start_bot')
-		bot.polling(timeout=30)
-	except Exception as e:
-		print(e.args)
-		sleep(10)
-		print('start_bot_2')
-		bot.polling(timeout=30)
-
-
+if __name__ == '__main__':
+    try:
+        print('start_bot')
+        bot.polling(timeout=30)
+    except Exception as e:
+        print(e.args)
+        sleep(10)
+        print('start_bot_2')
+        bot.polling(timeout=30)
